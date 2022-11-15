614dbbd6b022f58482207669f5f33240f646f1c0
Mike <winiar93@gmail.com>
2022-11-15 21:06:39 +0100

    added fastapi test script

--- otodom_scrape.py
+++ otodom_scrape.py
@@ -3,7 +3,11 @@ import time
 import pandas as pd
 
 def clear_txt(text):
-    return int(''.join(filter(str.isdigit,text.replace("²",""))))
+
+    try:
+        return int(''.join(filter(str.isdigit,text.replace("²",""))))
+    except ValueError: 
+        return 0 
 
 data_records = []
 
@@ -14,7 +18,7 @@ def get_page_content():
         context = browser.new_context()
         page = context.new_page()
 
-        for page_number in range(1,5):
+        for page_number in range(1,15):
 
             page.goto(f"https://www.otodom.pl/pl/wyszukiwanie/sprzedaz/mieszkanie/malopolskie/wielicki/wieliczka?page={page_number}&limit=10&market=ALL&distanceRadius=0&priceMin=300000&priceMax=400000&by=PRICE&direction=ASC")
             time.sleep(1)
@@ -29,7 +33,10 @@ def get_page_content():
                 url = f.query_selector('xpath=//a[@data-cy="listing-item-link"]').get_attribute("href")
                 url = "https://www.otodom.pl"+url
                 pirce = clear_txt(f.query_selector("span[class*='css-s8wpzb eclomwz2']").inner_text())
-                price_per_m2 = clear_txt(f.query_selector("span[class*='css-s8wpzb eclomwz2'] strong").inner_text())
+                try:
+                    price_per_m2 = clear_txt(f.query_selector("span[class*='css-s8wpzb eclomwz2'] strong").inner_text())
+                except AttributeError:
+                    price_per_m2 = 0
                 square_m = float(f.query_selector("span[class*='css-s8wpzb eclomwz2']:nth-child(4)").inner_text().replace("m²","").strip())
                 location = f.query_selector('//p[@class="css-80g06k es62z2j12"]').inner_text().replace(",","")
 
@@ -42,7 +49,7 @@ get_page_content()
 df = pd.DataFrame(data_records, columns = ['URL', 'Price', 'Pln/m2', 'Square meters', 'location'])
 print(df.shape)
 print(df.head())
-df.drop_duplicates()
+df.drop_duplicates(subset=['URL'])
 df = df.sort_values(['Price', 'Square meters'],
               ascending = [True, True])
 df.to_csv("ceny_mieszkan.csv", sep=';', index=False)

f381a938b549ae7d33386b75e70cde1c7874a539
Mike <winiar93@gmail.com>
2022-11-19 00:19:06 +0100

    selenium conf

--- otodom.py
+++ otodom.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -6,8 +7,12 @@ import time
 import re
 import pandas as pd
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 
 def clear_txt(text):

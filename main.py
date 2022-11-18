c3282281d06e1c27b5ea70de437735ca2743de5b
Mike <winiar93@gmail.com>
2022-11-19 00:00:46 +0100

    changes in main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from otodom_scrape import get_page_content
+from otodom import get_data
 import asyncio
 app = FastAPI()
 
 
 @app.get("/")
 def root():
-    return get_page_content()
+    return get_data()

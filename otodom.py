ec1e14ce1b267261e3959384b47f8ac60516ae81
Mike <winiar93@gmail.com>
2022-11-19 00:55:08 +0100

    changes in otodom.py

--- otodom.py
+++ otodom.py
@@ -12,7 +12,8 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+#driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+driver = webdriver.Chrome("/.chromedriver/bin/chromedriver", options=chrome_options)
 
 
 def clear_txt(text):

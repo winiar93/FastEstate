1bafa03338a41a91ab487a5119fd460f27e2e4dc
Mike <winiar93@gmail.com>
2022-11-18 23:57:11 +0100

    test selenium

---
+++ otodom.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+import time
+import re
+import pandas as pd
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+
+def clear_txt(text):
+
+    try:
+        if "Zapytaj" in text:
+            return text
+        else:
+            text_str = "".join(text.split()).split("z")[0].replace("m²","")
+            return text_str
+    except ValueError: 
+        return 0 
+
+def get_data():
+    website = 'https://www.otodom.pl/pl/wyszukiwanie/sprzedaz/mieszkanie/malopolskie/wielicki/wieliczka?page=1&limit=400&market=ALL&distanceRadius=0&priceMin=300000&priceMax=450000&by=PRICE&direction=ASC'
+    path = 'chromedriver.exe'
+    driver = webdriver.Chrome(path, chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.get(website)
+
+    time.sleep(2)
+    cookies_button = driver.find_element(by=By.XPATH, value='//button[@id="onetrust-accept-btn-handler"]')
+    cookies_button.click()
+    time.sleep(2)
+
+    location = []
+    prices = []
+    m2_price = []
+    rooms = []
+    m2 = []
+    urls = []
+
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1)
+
+    loc = driver.find_elements(by=By.XPATH, value='//p[@class="css-80g06k es62z2j12"]')
+    details = driver.find_elements(by=By.XPATH, value='//span[@class="css-s8wpzb eclomwz2"]')
+    links = driver.find_elements(by=By.XPATH, value='//a[@data-cy="listing-item-link"]')
+
+    # create a list of locations
+    for t in loc:
+        location.append(t.text)
+
+    # create a list of listing details
+    x = 0
+    while x < len(details):
+        prices.append(details[x].text)
+        m2_price.append(details[x+1].text)
+        rooms.append(details[x + 2].text)
+        m2.append(details[x + 3].text)
+        x += 4
+
+    for l in links:
+        urls.append(l.get_attribute('href'))
+
+    df = pd.DataFrame(list(zip(location, prices, m2_price, m2, rooms, urls)),
+                columns =['Location', 'Price', 'Price per m2', 'Size M2', 'Rooms', 'URL'])
+    df.drop_duplicates(subset=['URL'])
+
+    df['Price'] = df['Price'].apply(clear_txt)
+    df['Price per m2'] = df['Price per m2'].apply(clear_txt)
+    df['Size M2'] = df['Size M2'].apply(clear_txt)
+
+    df = df.sort_values(['Price', 'Size M2'],
+                ascending = [True, True])
+    output = df.to_dict("record")     
+
+    driver.quit()
+    return  output

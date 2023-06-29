72ed8ab1697e8907230339972fe03b6f38614f63
winiar93 <winiar93@gmail.com>
2023-06-29 22:22:55 +0200

    updated sql query

--- sql_scripts/offer_ranking.sql
+++ sql_scripts/offer_ranking.sql
@@ -1,4 +1,3 @@
-
  WITH min_max_prices AS (
   SELECT
     MIN(total_price) AS min_price,
@@ -16,16 +15,18 @@ min_max_areas AS (
 normalized_values AS (
   select
     offer_id,
+	updated_at,
   offer_url,
   street,
     (total_price - min_price) / (max_price - min_price) AS normalized_total_price,
     (area_square_meters - min_area) / (max_area - min_area) AS normalized_area,
     CASE
-	WHEN rooms_number = 'ONE' THEN 0.1
-    WHEN rooms_number = 'TWO' THEN 0.2
-    WHEN rooms_number = 'THREE' THEN 0.3
-    WHEN rooms_number = 'FOUR' THEN 0.4
-    when rooms_number = 'FIVE' THEN 0.5
+	-- individually provided weights
+	WHEN rooms_number = 'ONE' THEN 0.2
+    WHEN rooms_number = 'TWO' THEN 0.6
+    WHEN rooms_number = 'THREE' THEN 1
+    WHEN rooms_number = 'FOUR' THEN 1
+    when rooms_number = 'FIVE' THEN 0.2
     END AS normalized_rooms_number
   FROM
     public.flat_offers
@@ -33,17 +34,20 @@ normalized_values AS (
     min_max_prices
   CROSS JOIN
     min_max_areas
-)
-select
-  offer_id,
-  offer_url,
-  street,
-  1 - normalized_total_price as normalized_total_price,
-  normalized_area,
-  normalized_rooms_number,
-  1 - normalized_total_price + normalized_area + normalized_rooms_number as ranking
- 
+),
+
+flats_rank as (select
+offer_id,
+1 - normalized_total_price + normalized_area + normalized_rooms_number as ranking
 FROM
-  normalized_values;
- 
- 
+normalized_values
+where updated_at::date = NOW()::date)
+
+
+UPDATE public.flat_offers
+SET "rank" = flats_rank.ranking
+FROM flats_rank
+WHERE public.flat_offers.offer_id = flats_rank.offer_id;
+
+delete from public.flat_offers
+where "rank" is Null

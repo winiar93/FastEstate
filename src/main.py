9cc0b7b1826ab04c0ff8a7bc97ff045fe64da98e
winiar93 <winiar93@gmail.com>
2023-06-29 22:31:15 +0200

    update get_data endpoint

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
-from fastapi import FastAPI
-from otodom import PageScraper
 import json
 import logging
-from db_connector import get_sqlmodel_engine, insert_flat_offer, get_session
+
+from otodom import PageScraper
+from sql_models import FlatOffers
+
+from fastapi import FastAPI
+from fastapi import APIRouter, Depends, status, Response
+from fastapi.responses import JSONResponse
+
 from sqlmodel import Field, Session, SQLModel, create_engine, select, update
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.exc import IntegrityError
-from sql_models import FlatOffers
-from fastapi import APIRouter, Depends, status, Response
-from fastapi.responses import JSONResponse
-from sqlalchemy import text
+from sqlalchemy import text, desc
+
+from db_connector import get_sqlmodel_engine, insert_flat_offer, get_session
+
+
 
 
 engine = get_sqlmodel_engine()
@@ -57,24 +63,19 @@ async def insert_data(session: Session = Depends(get_session)):
     return output
 
 @app.get("/get_data")
-async def get_data(session: Session = Depends(get_session), page_size: int = 100, page: int = 0):
+async def get_data(session: Session = Depends(get_session), page_size: int = 10, page: int = 0, rank_order: bool = True):
     stmt = select(FlatOffers)
 
     if page_size:
         stmt = stmt.limit(page_size)
     if page: 
         stmt = stmt.offset(page*page_size)
+    if rank_order:
+        stmt = stmt.order_by(desc(FlatOffers.rank))
     
     data = session.exec(stmt).all()
     
-
     reject_list  = ['investment_estimated_delivery', 'created_at', 'updated_at']
 
     content = [{key: value for key, value in d.__dict__.items() if key not in reject_list} for d in data]
     return content
-    
-
-@app.get("/best_offer")
-async def top_offer():
-
-    return {'test': 111}

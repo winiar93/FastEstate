f0ee9deb74776aeb13abd87ae0932b60e7ba572e
winiar93 <winiar93@gmail.com>
2023-07-01 20:51:40 +0200

    update psql connector

--- src/db_connector.py
+++ src/db_connector.py
@@ -3,9 +3,14 @@ from sqlalchemy import select, text
 from typing import List, Dict, Any
 from sqlmodel import Session, SQLModel, create_engine
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.exc import SQLAlchemyError
+
 from sql_models import FlatOffers
 import logging
 from datetime import datetime
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
 
 pg_user = "postgres"
 pg_dbname = "postgres"
@@ -16,38 +21,62 @@ pg_con_string = (
     f"postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_dbname}"
 )
 
-def insert_flat_offer(engine, item):
-    conn = engine.connect()
-    stmt = (
-        insert(FlatOffers)
-        .values(item)
-    )
+class DBConnector:
+    def __init__(self,
+        pg_user = "postgres",
+        pg_dbname = "postgres",
+        pg_password = "Welcome1",
+        pg_host = "postgres",
+        pg_port = 5432) -> None:
 
-    stmt = stmt.on_conflict_do_update(
-        index_elements=[FlatOffers.offer_id],
-        set_={'price_per_square_meter': item['price_per_square_meter'], 
-              'total_price': item['total_price'],
-              'updated_at': datetime.now()},
-    )
+        self.pg_user = pg_user
+        self.pg_dbname = pg_dbname
+        self.pg_password = pg_password
+        self.pg_host = pg_host
+        self.pg_port = pg_port
+        self.pg_con_string = (
+            f"postgresql+psycopg2://{self.pg_user}:{self.pg_password}@{self.pg_host}:{self.pg_port}/{self.pg_dbname}"
+        )
+        self.engine = self.get_sqlmodel_engine()
 
-    conn.execute(stmt)
-    conn.commit()
-    conn.close()
+
+    def get_sqlmodel_engine(self):
+        engine = create_engine(pg_con_string)
+        return engine
+
+    def insert_flat_offer(self, item):
+        try:
+            conn = self.engine.connect()
+            stmt = (
+                insert(FlatOffers)
+                .values(item)
+            )
+
+            stmt = stmt.on_conflict_do_update(
+                index_elements=[FlatOffers.offer_id],
+                set_={'price_per_square_meter': item['price_per_square_meter'], 
+                    'total_price': item['total_price'],
+                    'updated_at': datetime.now()},
+            )
+            
+            conn.execute(stmt)
+            logging.info(f'Item with id = {item.get("offer_id")} upserted.')
+            conn.commit()
+            conn.close()
+        except SQLAlchemyError as e:
+            logging.warning(f'Error in processing data: \n {e} \n Item: {item}')
 
 
-def get_sqlmodel_engine():
-    engine = create_engine(pg_con_string)
-    return engine
+    def get_session(self):
+        engine = self.get_sqlmodel_engine()
+        with Session(engine) as session:
+            yield session
 
-def get_session():
-    engine = get_sqlmodel_engine()
-    with Session(engine) as session:
-        yield session
-
-def test():
-    engine = get_sqlmodel_engine()
-    conn = engine.connect() 
-    logging.info('ok')
-    conn.close()
+    def test(self):
+        engine = self.get_sqlmodel_engine()
+        conn = engine.connect() 
+        logging.info('ok')
+        conn.close()
+        
 
 
--- src/main.py
+++ src/main.py
@@ -13,41 +13,42 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import text, desc
 import pandas as pd
-from db_connector import get_sqlmodel_engine, insert_flat_offer, get_session
+from db_connector import DBConnector
 
+import logging
 
+logging.getLogger().setLevel(logging.INFO)
 
-
-engine = get_sqlmodel_engine()
+db = DBConnector()
+engine = db.get_sqlmodel_engine()
+db_session = db.get_session
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-
-
 app = FastAPI()
-ps = PageScraper()
+
 
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
-# http://localhost:8000/docs
-
 @app.get("/scrape_data", status_code=200)
-async def insert_data(session: Session = Depends(get_session)):
-
+async def insert_data(session: Session = Depends(db_session), min_price: int = 300000, max_price: int = 450000):
+    ps = PageScraper(min_price=min_price,max_price = max_price)
+    logging.info(f'Running web page scraper ...')
     data = ps.run()
     inserted_enities_cnt = 0
+    logging.info(f'Updating database.')
     for item in data:
         try:
-            insert_flat_offer(engine, item)
+            db.insert_flat_offer(item)
             inserted_enities_cnt += 1
 
         except Exception as err:
             logging.warning(f'Operation failed: \n {err}')
 
-
+    logging.info(f'Updating offers ranking.')
     with open("./sql_scripts/offer_ranking.sql") as file:
         query = text(file.read())
         session.execute(query)
@@ -63,7 +64,7 @@ async def insert_data(session: Session = Depends(get_session)):
     return output
 
 @app.get("/get_data")
-async def get_data(session: Session = Depends(get_session), page_size: int = 10, page: int = 0, rank_order: bool = True):
+async def get_data(session: Session = Depends(db_session), page_size: int = 10, page: int = 0, rank_order: bool = True):
     stmt = select(FlatOffers)
 
     if page_size:
@@ -84,8 +85,9 @@ async def get_data(session: Session = Depends(get_session), page_size: int = 10,
 async def get_data():
     file_name ='offers_of_flats.csv'
     stmt = select(FlatOffers)
-    engine = get_sqlmodel_engine()
     df = pd.read_sql_query(stmt, con=engine)
+    logging.info(f'Data successfully loaded.')
     df.to_csv(file_name, sep='\t')
     engine.dispose()
+    logging.info(f'Output saved into csv file.')
     return FileResponse(file_name, media_type='application/octet-stream', filename=file_name)
